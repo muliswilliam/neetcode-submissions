4f8cce8f1616ecd358895732c84ba78387b5cdb8
William Muli <48117473+muliswilliam@users.noreply.github.com>
2026-08-28 22:26:36 +0300

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        const boardSize = 9
+        const rows = Array.from({ length: 9 }, () => new Set());
+        const cols = Array.from({ length: 9 }, () => new Set());
+        const squares = Array.from({ length: 9 }, () => new Set());
+
+        for (let r = 0; r < boardSize; r++) {
+            for (let c = 0; c < boardSize; c++) {
+                const current = board[r][c]
+
+                if (current === ".") continue
+                
+                const squareIndex = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+
+                if (rows[r].has(current) || cols[c].has(current) ||                    squares[squareIndex].has(current)) {
+                    return false
+                }
+
+                rows[r].add(current)
+                cols[c].add(current)
+                squares[squareIndex].add(current)
+            }
+        }
+
+        return true
+    }
+
+    getSqKey(r, c) {
+        return `${r},${c}`
+    }
+}
